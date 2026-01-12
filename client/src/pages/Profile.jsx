85494a96067a0ee2f87f57ca5d7cf8cd3bbfbf8b
Ayesha Tariq <ashu35201@gmail.com>
2026-01-12 23:42:59 +0500

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import axios from 'axios';
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, } from '../redux/user/userSlice.js';
+import {Link} from 'react-router-dom';
 import {useDispatch} from 'react-redux'; 
 
 function Profile() {
@@ -173,6 +174,9 @@ const handleSubmit = async (e) => {
         <button disabled={loading} type="submit" className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className="bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95" to={"/create-listing"} >
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
