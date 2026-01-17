f1ce4006f626fbfdf45dd8acb356c41f0ff6892a
Ayesha Tariq <ashu35201@gmail.com>
2026-01-17 17:18:33 +0500

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -132,6 +132,25 @@ const handleSubmit = async (e) => {
       setShowListingsError(true);
     }
   };
+  
+   const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -233,6 +252,7 @@ const handleSubmit = async (e) => {
 
               <div className='flex flex-col item-center'>
                 <button
+                  onClick={() => handleListingDelete(listing._id)}
                   className='text-red-700 uppercase'
                 >
                   Delete
