47c0c91d2cc812c5e23010b8b3d1510772822a79
Ayesha Tariq <ashu35201@gmail.com>
2026-01-19 16:11:35 +0500

    Create the listing item component and show listings

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+import lineClamp from '@tailwindcss/line-clamp'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [lineClamp],
+}
